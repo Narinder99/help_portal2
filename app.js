d9ac663ed87c4b0c04eccaa3fc5198f1e2ca889c
Narinder <narindersaini140@gmail.com>
2023-01-09 02:21:09 +0530

    main app

--- app.js
+++ app.js
@@ -16,72 +16,72 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 3000;
 
-//remove code from  here
-const server=http.createServer((req,res) => {
-    res.statusCode=200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('Zeet Node');
-});
+// //remove code from  here
+// const server=http.createServer((req,res) => {
+//     res.statusCode=200;
+//     res.setHeader('Content-Type','text/plain');
+//     res.end('Zeet Node');
+// });
 
-server.listen(port,hostname,()=> {
-    console.log("server runnning");
+// server.listen(port,hostname,()=> {
+//     console.log("server runnning");
+// })
+// // till here
+
+import mongoosei from 'mongoose';
+const mongoose=mongoosei
+import {mongoUrl} from './string.js'
+
+import ejsi from 'ejs';
+const ejs=ejsi;
+
+//app initallize
+//app.use('/public', express.static('public'))
+console.log(__dirname)
+//app.use(express.static(path.join(__dirname, 'css')))
+
+app.use(express.static(__dirname)); //Add this line. Change public to whatever location your static assets are stored at
+
+app.use(express.urlencoded())
+app.set('views', path.join(__dirname, 'view'));
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+
+// database connection
+mongoose.connect(mongoUrl);
+const con=mongoose.connection;
+con.on('open',()=>{
+    console.log("connected ")
 })
-// till here
 
-// import mongoosei from 'mongoose';
-// const mongoose=mongoosei
-// import {mongoUrl} from './string.js'
+app.use(express.json())
 
-// import ejsi from 'ejs';
-// const ejs=ejsi;
+// area router
+import AreaRouter from './routes/area.js'
+app.use('/area',AreaRouter);
 
-// //app initallize
-// //app.use('/public', express.static('public'))
-// console.log(__dirname)
-// //app.use(express.static(path.join(__dirname, 'css')))
+// department router
+import DepartmentRouter from './routes/departments.js'
+app.use('/department',DepartmentRouter);
 
-// app.use(express.static(__dirname)); //Add this line. Change public to whatever location your static assets are stored at
+//submitData
+import SubmitDataRouter from './routes/submitData.js'
+app.use('/submit',SubmitDataRouter)
 
-// app.use(express.urlencoded())
-// app.set('views', path.join(__dirname, 'view'));
-// app.engine('html', ejs.renderFile);
-// app.set('view engine', 'html');
+//update Data
+import updateDataRouter from './routes/updateData.js'
+app.use('/update',updateDataRouter)
 
-// // database connection
-// mongoose.connect(mongoUrl);
-// const con=mongoose.connection;
-// con.on('open',()=>{
-//     console.log("connected ")
-// })
+app.get('/',(req,res)=>{
+    const params={};
+    res.status(200).render('index.html',params);
+})
+app.get('/verification',(req,res)=>{
+    //const params={};
+    res.status(200).render('verification.html');
+})
 
-// app.use(express.json())
-
-// // area router
-// import AreaRouter from './routes/area.js'
-// app.use('/area',AreaRouter);
-
-// // department router
-// import DepartmentRouter from './routes/departments.js'
-// app.use('/department',DepartmentRouter);
-
-// //submitData
-// import SubmitDataRouter from './routes/submitData.js'
-// app.use('/submit',SubmitDataRouter)
-
-// //update Data
-// import updateDataRouter from './routes/updateData.js'
-// app.use('/update',updateDataRouter)
-
-// app.get('/',(req,res)=>{
-//     const params={};
-//     res.status(200).render('index.html',params);
-// })
-// app.get('/verification',(req,res)=>{
-//     //const params={};
-//     res.status(200).render('verification.html');
-// })
-
-// app.listen(port,()=>{
-//     console.log(`Application is running on a ${port}`);
-// })
+app.listen(port,()=>{
+    console.log(`Application is running on a ${port}`);
+})
 
