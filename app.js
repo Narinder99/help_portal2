7185fce353bf50e34ada50e16bc6741b86f8c7db
Narinder <narindersaini140@gmail.com>
2023-01-09 02:13:41 +0530

    comment every thing

--- app.js
+++ app.js
@@ -28,60 +28,60 @@ server.listen(port,hostname,()=> {
 })
 // till here
 
-import mongoosei from 'mongoose';
-const mongoose=mongoosei
-import {mongoUrl} from './string.js'
+// import mongoosei from 'mongoose';
+// const mongoose=mongoosei
+// import {mongoUrl} from './string.js'
 
-import ejsi from 'ejs';
-const ejs=ejsi;
+// import ejsi from 'ejs';
+// const ejs=ejsi;
 
-//app initallize
-//app.use('/public', express.static('public'))
-console.log(__dirname)
-//app.use(express.static(path.join(__dirname, 'css')))
+// //app initallize
+// //app.use('/public', express.static('public'))
+// console.log(__dirname)
+// //app.use(express.static(path.join(__dirname, 'css')))
 
-app.use(express.static(__dirname)); //Add this line. Change public to whatever location your static assets are stored at
+// app.use(express.static(__dirname)); //Add this line. Change public to whatever location your static assets are stored at
 
-app.use(express.urlencoded())
-app.set('views', path.join(__dirname, 'view'));
-app.engine('html', ejs.renderFile);
-app.set('view engine', 'html');
+// app.use(express.urlencoded())
+// app.set('views', path.join(__dirname, 'view'));
+// app.engine('html', ejs.renderFile);
+// app.set('view engine', 'html');
 
-// database connection
-mongoose.connect(mongoUrl);
-const con=mongoose.connection;
-con.on('open',()=>{
-    console.log("connected ")
-})
+// // database connection
+// mongoose.connect(mongoUrl);
+// const con=mongoose.connection;
+// con.on('open',()=>{
+//     console.log("connected ")
+// })
 
-app.use(express.json())
+// app.use(express.json())
 
-// area router
-import AreaRouter from './routes/area.js'
-app.use('/area',AreaRouter);
+// // area router
+// import AreaRouter from './routes/area.js'
+// app.use('/area',AreaRouter);
 
-// department router
-import DepartmentRouter from './routes/departments.js'
-app.use('/department',DepartmentRouter);
+// // department router
+// import DepartmentRouter from './routes/departments.js'
+// app.use('/department',DepartmentRouter);
 
-//submitData
-import SubmitDataRouter from './routes/submitData.js'
-app.use('/submit',SubmitDataRouter)
+// //submitData
+// import SubmitDataRouter from './routes/submitData.js'
+// app.use('/submit',SubmitDataRouter)
 
-//update Data
-import updateDataRouter from './routes/updateData.js'
-app.use('/update',updateDataRouter)
+// //update Data
+// import updateDataRouter from './routes/updateData.js'
+// app.use('/update',updateDataRouter)
 
-app.get('/',(req,res)=>{
-    const params={};
-    res.status(200).render('index.html',params);
-})
-app.get('/verification',(req,res)=>{
-    //const params={};
-    res.status(200).render('verification.html');
-})
+// app.get('/',(req,res)=>{
+//     const params={};
+//     res.status(200).render('index.html',params);
+// })
+// app.get('/verification',(req,res)=>{
+//     //const params={};
+//     res.status(200).render('verification.html');
+// })
 
-app.listen(port,()=>{
-    console.log(`Application is running on a ${port}`);
-})
+// app.listen(port,()=>{
+//     console.log(`Application is running on a ${port}`);
+// })
 
