c5f984cd109bad1dd4b23ad4143a8c02f8f54eb3
Narinder Singh <50241887+Narinder99@users.noreply.github.com>
2024-05-10 16:29:57 +0530

    Delete string.js

--- string.js
+++
@@ -1,8 +0,0 @@
-//export const mongoUrl= "mongodb://localhost/HelpPortal";
-export const mongoUrl="mongodb+srv://NarinderSingh:Singh7589368295@cluster0.62xcepu.mongodb.net/HelpPortal?retryWrites=true&w=majority"
-export const areaApiUrl="https://help-portal2-4gik-main-vplgaqghsq-wm.a.run.app/area" // our website link
-export const departmentNamesApiUrl="https://help-portal2-4gik-main-vplgaqghsq-wm.a.run.app/department"
-export const areaWiseDataStoreUrl="https://help-portal2-4gik-main-vplgaqghsq-wm.a.run.app/submit/areaData"
-export const departmentWiseDataStoreUrl="https://help-portal2-4gik-main-vplgaqghsq-wm.a.run.app/submit/departmentData"
-export const otherDataStoreUrl="https://help-portal2-4gik-main-vplgaqghsq-wm.a.run.app/submit/otherData"
-
